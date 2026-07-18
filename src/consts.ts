86d4055d363c8ea60a4a0f6802210839708efb99
bumpfuji10 <bumpfuji10@gmail.com>
2026-07-18 13:09:25 +0900

    Polish blog design

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 // サイト全体で使用するグローバル設定
 
-export const SITE_TITLE = 'Kyam Blog';
-export const SITE_DESCRIPTION = '技術ブログ';
+export const SITE_TITLE = 'kyam.net';
+export const SITE_DESCRIPTION = '技術と日々の学びを書き留めるノート';
